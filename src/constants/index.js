9efd4dbab01c83a8cedd20ef501776a67ccc25e2
= <awirasakti18@gmail.com>
2023-12-05 09:38:21 +0700

    2118104 Andhika Wira S B

---
+++ src/constants/index.js
@@ -0,0 +1,5 @@
+const baseUrl = 'https://api.themoviedb.org/3/';
+const posterImageUrl = 'https://image.tmdb.org/t/p/';
+const apiKey = '8d4087697f32d27704b459a3aeb81c5e';
+
+export {baseUrl, posterImageUrl, apiKey};
